0f897a130f741b5b251b60376f44de6a40d17e10
ruppuluri386 <uppuluriv@gmail.com>
2025-09-24 02:07:33 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -7,8 +7,8 @@ st.write("This app connects to a FastAPI calculator service.")
 
 # Define the API base URL
 
+#api_url =  "http://localhost:9321"
 api_url = "https://ru-api-example.onrender.com"
-
 
 # Initialize session state to store the calculator display and current operation
 if 'display' not in st.session_state:
